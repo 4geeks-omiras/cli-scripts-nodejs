556aca6ce095df25e0e82c7749015e2766919a94
omiras <oscar.miras@gmail.com>
2025-04-23 21:12:09 +0200

    done

--- 0-show-os-info.js
+++ 0-show-os-info.js
@@ -1,1 +1,15 @@
 // Lee el README
+// Include os module and create its object
+const os = require('os');
+
+// Obtener nombre del equipo
+const hostname = os.hostname();
+console.log(`Nombre del equipo: ${hostname}`);
+
+// Obtener versión del sistema operativo
+const osVersion = os.version();
+console.log(`Edición/Versión del sistema operativo: ${osVersion}`);
+
+// Obtener memoria RAM total en GB
+const totalMemoryGB = (os.totalmem() / (1024 ** 3)).toFixed(2);
+console.log(`Memoria RAM total: ${totalMemoryGB} GB`);
