ce4b46e37f92c45efcbc964e75681a89a91ae104
omiras <oscar.miras@gmail.com>
2025-04-23 21:52:37 +0200

    done

--- 2-throw-custom-dice.js
+++ 2-throw-custom-dice.js
@@ -1,1 +1,9 @@
 // ¡Buena suerte!
+
+// leer el argumento que nos va a pasar el usuario
+const topNumber = process.argv[2];
+
+// Generar un número aleatorio entre 1 y topNumber
+const randomNumber = Math.floor(Math.random() * topNumber) + 1;
+
+console.log(randomNumber);
